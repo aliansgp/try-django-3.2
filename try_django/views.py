b35fe08880d9680eed705f6320be48aad1b4a1a5
aliansgp <aliansgp@gmail.com>
2021-09-13 01:18:54 +0430

    'add-home-to-view-and-simple-HTMLresponse'

--- try_django/views.py
+++ try_django/views.py
@@ -1,3 +1,15 @@
 """
 to render HTML web pages
 """
+from django.http import HttpRequest
+
+HTML_STRING="""
+<h1>Hello World</h1>
+"""
+
+def home(request):
+    """
+    take in a request:
+    returns HTML as response
+    """
+    return HttpRequest(HTML_STRING)
