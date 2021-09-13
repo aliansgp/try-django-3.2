39f8dc9d92058e7d1b710baf8705827c7068e57b
saly abdollahian <38432834+aliansgp@users.noreply.github.com>
2021-09-13 21:42:25 +0430

    'adding-Article-model-to-view-and-show-data-from-model'

--- try_django/views.py
+++ try_django/views.py
@@ -2,14 +2,24 @@
 to render HTML web pages
 """
 from django.http import HttpResponse
+from article.models import Article
+import random
 
-HTML_STRING="""
-<h1>Hello World</h1>
-"""
+
 
 def home(request):
     """
     take in a request:
     returns HTML as response
     """
+    random_id = random.randint(1,3)
+    data_Article = Article.objects.get(id=random_id)
+    title =f"""
+    <h1>{data_Article.title}</h1>
+    """
+    content = f"""
+    <p>{data_Article.content}</p>
+    """
+    HTML_STRING = title + content
+
     return HttpResponse(HTML_STRING)
